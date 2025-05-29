ca9113d751683c4cda017b822a396e38fd2ae550
kaique <kai_que15@hotmail.com>
2025-05-28 21:22:16 -0300

    deploy strapi/postgree 2

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -8,6 +8,18 @@ module.exports = [
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       headers: '*',
       credentials: true,
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://cmt-tawny.vercel.app", // frontend em produção
+            "https://strapi-n86r.onrender.com" // backend
+          ],
+        },
+      },
     },
   },
   'strapi::poweredBy',
